bc301bab264b01e759035e2a74363df48ce7c467
rattikan <rattikan.queen@gmail.com>
2017-12-19 09:28:49 +0700

    Test Mesthods

---
+++ src/oop_lab4/TestMethods.java
@@ -0,0 +1,29 @@
+package oop_lab4;
+
+public class TestMethods {
+    //method
+    //type 1
+    public static void A(){
+        System.out.println("Hello A");
+        B(20);
+    }//A
+//   type 2
+    public static void B(int x){
+        System.out.println("Hello B" + x);
+    }//B
+ // type 3
+    public static int C (int x,int y){
+        System.out.println("Hello C");
+        return x+y;
+    }   //C
+
+    public static void main(String[] args) {
+        System.out.println("Hello main");
+        // calling method
+       // A();
+      //  B(10);
+        int number = C(10,20);
+        System.out.println(number);
+        System.out.println(C(50,50));
+    }//mainA
+}//class
